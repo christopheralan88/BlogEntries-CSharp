3d7b42335f605d405b6b5d5aedbc7f57c1a33560
christopheralan88 <christopheralan88@gmail.com>
2017-08-15 01:25:03 -0400

    Basic Index view worked

---
+++ BlogEntries-CSharp/Controllers/BlogEntryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogEntries_CSharp.Controllers
+{
+    public class BlogEntryController : Controller
+    {
+
+
+        // GET: BlogEntry
+        public string Index()
+        {
+            //return View();
+            return "Hello world!";
+        }
+    }
+}
